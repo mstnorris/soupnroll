f422609b7d5e3fec9986b0622d4fd9e7a1ac163e
Michael Norris <github@michaelnorris.co.uk>
2015-08-17 15:24:09 +0100

    add copytext to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Soup n Roll</div>
             </div>
         </div>
     </body>
